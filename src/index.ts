29ea057700466138b69fc3014de880e48a014f96
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-08 22:54:29 -0600

    Tipos de datos y el tipo any

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+let message: string = "Hello world!";
+message = "bye!";
+console.log(message);
+/**
+ * Tipos de datos JS
+ * * number
+ * * string
+ * * boolean
+ * * null
+ * * undefined
+ * * object
+ * * function
+ * Tipos de TS
+ * * any
+ * * unknown
+ * * never
+ * * arrays
+ * * tuplas
+ * * Enums
+ * Tipos inferidos
+ */
+
+let extincionDinosaurios: number = 76_000_000;
+let dinosaurioFavorito: string = "T-Rex";
+let extintos: boolean = true;
+let variable;
+variable = "String";
+variable = 42;
+
+function process(config: any) {
+  return config;
+}
