105b1f23e2cb2225986797d95f30184f551e2b83
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-12 21:50:34 -0600

    Herencia

---
+++ src/Herencia.ts
@@ -0,0 +1,54 @@
+class BasicData {
+  constructor(
+    public name: string,
+    public description: string,
+    public created_at: Date,
+    public created_by: number
+  ) {}
+  get fullYear() {
+    return this.created_at.getFullYear();
+  }
+}
+
+class Product extends BasicData {
+  constructor(
+    public stock: number,
+    public sku: number,
+    name: string,
+    description: string,
+    created_at: Date,
+    created_by: number
+  ) {
+    super(name, description, created_at, created_by);
+  }
+}
+
+const product = new Product(
+  100,
+  1,
+  "iPhone",
+  "This is a smartphone",
+  new Date(),
+  1
+);
+
+class Category extends BasicData {
+  public products: Product[] = [];
+  constructor(
+    name: string,
+    description: string,
+    created_at: Date,
+    created_by: number
+  ) {
+    super(name, description, created_at, created_by);
+  }
+
+  addProduct(product: Product) {
+    this.products.push(product);
+  }
+}
+
+const category = new Category("Phones", "", new Date(), 1);
+category.addProduct(product);
+
+console.log(product, category);
