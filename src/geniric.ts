334404e7044cae5ab3cf829a9daa9aed9e43ad34
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-13 22:16:17 -0600

    Genericos en interfaces

--- src/geniric.ts
+++ src/geniric.ts
@@ -42,3 +42,25 @@ const developer_two = new Developer<string>("Hello world!");
 
 developer.computer;
 developer_two.computer.toLocaleUpperCase();
+
+interface KeyValue<T, V> {
+  key: T;
+  value: V;
+}
+
+interface ProductOne {
+  id: string;
+}
+
+function fetchProduct(): KeyValue<string, ProductOne> {
+  return {
+    key: "product id",
+    value: { id: "product id" },
+  };
+}
+function fetchStockt(): KeyValue<string, number> {
+  return {
+    key: "product id",
+    value: 45,
+  };
+}
