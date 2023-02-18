cddd8f4d4edf1772f951f1b17ce0ee0f78be5597
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-18 16:13:30 -0600

    useState

--- hello-react/src/components/Input.tsx
+++ hello-react/src/components/Input.tsx
@@ -2,11 +2,13 @@ import { ChangeEventHandler } from "react"
 
 interface InputProps {
     placeholder: string,
-    handleChange: ChangeEventHandler<HTMLInputElement>
+    handleChange: ChangeEventHandler<HTMLInputElement>,
+    name: string
 }
-export default function Input({placeholder, handleChange}: InputProps) {
+export default function Input({placeholder, handleChange, name }: InputProps) {
     return (
         <input
+            name={name}
             className="input"
             placeholder={placeholder}
             onChange={handleChange}
--- hello-react/src/components/forms/User.tsx
+++ hello-react/src/components/forms/User.tsx
@@ -1,20 +1,30 @@
-import { ChangeEventHandler, MouseEventHandler } from "react"
+import { ChangeEventHandler, MouseEventHandler, useState } from "react"
 import Button from "../Button"
 import Input from "../Input"
+interface UserFormState {
+    name: string,
+    lastname: string
+}
 
+const InitialValue: UserFormState = {
+    name: '',
+    lastname: ''
+}
 export default function UserForm() {
+    const [form, setForm] = useState(InitialValue);
     const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        console.log(e.target.value)
+        const name = e.target.name as keyof UserFormState
+        setForm({...form, [name]: e.target.value})
     }
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
         e.preventDefault()
-        console.log("Hello world")
+        console.log(form)
     }
     return (
         <>
-            <Input placeholder='Nombre' handleChange={handleChange} />
-            <Input placeholder='Apellido' handleChange={handleChange} />
+            <Input name="name" placeholder='Nombre' handleChange={handleChange} />
+            <Input name="lastname" placeholder='Apellido' handleChange={handleChange} />
             <Button handleClick={handleClick}>Primary</Button>
         </>
     )
