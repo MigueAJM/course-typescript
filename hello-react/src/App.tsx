d6797f20d01258d538a0e24b6a80fca617a2a10e
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-18 15:26:08 -0600

    Integracion de react con TS y Bulma CSS: * npx create-react-app hello-react --template typescript * npm install -S Bulma

---
+++ hello-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button className="button is-primary">Hello world!</button>
+    </div>
+  );
+}
+
+export default App;
